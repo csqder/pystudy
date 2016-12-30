24953e29e19adac735cba8d8dcd5770457203ffe
karlcodesmith <karl.codesmith@gmail.com>
2016-12-30 17:51:18 +0800

    .

--- gen_dict.py
+++ gen_dict.py
@@ -1,5 +1,38 @@
-
+import random
+import time
 
 def generate_string_dict( n ):
-	s = set()
+	bowl = set()
+	while len(bowl)<n:
+		s = ''
+		lenth = random.randint(3,15)
+		for i in range(lenth):
+			s += chr(random.randint(33,126))
+		bowl.add(s)
+		s = ''
+	return bowl
+
+def read_dict_from_file( fn ):
+	d = {}
+	f = open(fn, 'r')
+	for line in f:
+		k,v = line.split(' ')
+		d[k] = v
+	f.close()
+	return d
+
+if __name__=='__main__':
+	t0 = time.time()
+	#s = generate_string_dict(2000000)
+	#l = list(s)
+	d = read_dict_from_file('out.txt')
+	td = time.time()-t0
+	print( td )
+	# f = open('out.txt','w')
+	# for i in range(0,len(l),2):
+	# 	f.write('%s %s\n'%(l[i], l[i+1]))
+	# f.close()
+	input('done!')	
+
+
 
